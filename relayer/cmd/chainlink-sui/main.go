ba1d26ba32a13ff302d1fd8e28932f318cbb6379
Joao Vasques <JoaoVasques@users.noreply.github.com>
2025-05-05 14:49:11 +0100

    Feature 97/implement sui relayer entrypoint (#110)
    * Refactor chainreader to use signer address from config
    
    * logging cleanup
    
    * Add Sui relayer plugin entrypoint
    
    * clenaup

---
+++ relayer/cmd/chainlink-sui/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/pelletier/go-toml/v2"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/loop"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/core"
+
+	suiplugin "github.com/smartcontractkit/chainlink-sui/relayer/plugin"
+)
+
+const (
+	loggerName = "PluginSui"
+)
+
+func main() {
+	s := loop.MustNewStartedServer(loggerName)
+	defer s.Stop()
+
+	p := &pluginRelayer{Plugin: loop.Plugin{Logger: s.Logger}}
+	defer s.Logger.ErrorIfFn(p.Close, "Failed to close")
+
+	s.MustRegister(p)
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: loop.PluginRelayerHandshakeConfig(),
+		Plugins: map[string]plugin.Plugin{
+			loop.PluginRelayerName: &loop.GRPCPluginRelayer{
+				PluginServer: p,
+				BrokerConfig: loop.BrokerConfig{
+					StopCh:   stopCh,
+					Logger:   s.Logger,
+					GRPCOpts: s.GRPCOpts,
+				},
+			},
+		},
+		GRPCServer: s.GRPCOpts.NewServer,
+	})
+}
+
+type pluginRelayer struct {
+	loop.Plugin
+}
+
+var _ loop.PluginRelayer = &pluginRelayer{}
+
+func (c *pluginRelayer) NewRelayer(ctx context.Context, config string, keystore loop.Keystore, capRegistry core.CapabilitiesRegistry) (loop.Relayer, error) {
+	d := toml.NewDecoder(strings.NewReader(config))
+	d.DisallowUnknownFields()
+
+	var cfg suiplugin.TOMLConfig
+
+	if err := d.Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to decode config toml: %w:\n\t%s", err, config)
+	}
+
+	if err := cfg.ValidateConfig(); err != nil {
+		return nil, fmt.Errorf("invalid tron config: %w", err)
+	}
+	if !cfg.IsEnabled() {
+		return nil, fmt.Errorf("cannot create new chain with ID %s: chain is disabled", *cfg.ChainID)
+	}
+
+	relayer, err := suiplugin.NewRelayer(&cfg, c.Logger, keystore)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create relayer: %w", err)
+	}
+
+	c.SubService(relayer)
+
+	return relayer, nil
+}
