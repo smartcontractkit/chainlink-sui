32f2bb6c7b4b2992a8902f3142921b00ac131a01
stackman27 <sishir.giri@smartcontract.com>
2025-09-17 00:41:09 -0700

    more logs

--- relayer/plugin/relayer.go
+++ relayer/plugin/relayer.go
@@ -104,11 +104,11 @@ func NewRelayer(cfg *config.TOMLConfig, lggr logger.Logger, keystore core.Keysto
 		return nil, fmt.Errorf("error in NewRelayer (monitor): %w", err)
 	}
 
-	loggerInstance.Info("TICKET VALUES1: ", *cfg.TransactionsIndexer.PollingIntervalSecs)
-	loggerInstance.Info("TICKET VALUES2: ", *cfg.TransactionsIndexer.PollingIntervalSecs)
+	loggerInstance.Info("TICKER VALUES1 TXINDEXER: ", *cfg.TransactionsIndexer.PollingIntervalSecs)
+	loggerInstance.Info("TICKER VALUES2 TXINDEXER: ", *cfg.TransactionsIndexer.PollingIntervalSecs)
 
-	loggerInstance.Info("TICKET VALUES3: ", *cfg.EventsIndexer.PollingIntervalSecs)
-	loggerInstance.Info("TICKET VALUES4: ", *cfg.EventsIndexer.PollingIntervalSecs)
+	loggerInstance.Info("TICKER VALUES3 EVENTSINDEXER: ", *cfg.EventsIndexer.PollingIntervalSecs)
+	loggerInstance.Info("TICKER VALUES4 EVENTSINDEXER: ", *cfg.EventsIndexer.PollingIntervalSecs)
 
 	// Setup indexers
 	txnIndexer := indexer.NewTransactionsIndexer(
