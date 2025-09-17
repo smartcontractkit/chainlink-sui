3c33e507b4e6a8c2c3d241347d75d597508ff5a0
faisal-link <faisal.altameemi@smartcontract.com>
2025-09-18 00:54:48 +0400

    update ptb client to include a cache and add a few helper methods

--- relayer/client/ptb_client.go
+++ relayer/client/ptb_client.go
@@ -15,20 +15,26 @@ import (
 	"github.com/block-vision/sui-go-sdk/signer"
 	"github.com/block-vision/sui-go-sdk/sui"
 	"github.com/block-vision/sui-go-sdk/transaction"
+	cache "github.com/patrickmn/go-cache"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/loop"
+	module_offramp "github.com/smartcontractkit/chainlink-sui/bindings/generated/ccip/ccip_offramp/offramp"
+	suiSigner "github.com/smartcontractkit/chainlink-sui/relayer/signer"
 	"golang.org/x/sync/semaphore"
 
+	"github.com/smartcontractkit/chainlink-sui/bindings/bind"
 	"github.com/smartcontractkit/chainlink-sui/relayer/codec"
 	"github.com/smartcontractkit/chainlink-sui/relayer/common"
 	"github.com/smartcontractkit/chainlink-sui/shared"
 )
 
 const (
-	maxCoinsPageSize uint   = 50
-	Base10           int    = 10
-	DefaultGasPrice  uint64 = 10_000
-	DefaultGasBudget uint64 = 1_000_000_000
+	maxCoinsPageSize            uint          = 50
+	Base10                      int           = 10
+	DefaultGasPrice             uint64        = 10_000
+	DefaultGasBudget            uint64        = 1_000_000_000
+	DefaultCacheExpiration      time.Duration = 120 * time.Minute
+	DefaultCacheCleanupInterval time.Duration = 240 * time.Minute
 )
 
 // var since it's passed via pointer
@@ -55,7 +61,14 @@ type SuiPTBClient interface {
 	LoadModulePackageIds(ctx context.Context, packageId string, module string, signerAddress string) ([]string, error)
 	GetLatestPackageId(ctx context.Context, packageId string, module string, signerAddress string) (string, error)
 	GetClient() sui.ISuiAPI
+	GetCache() *cache.Cache
+	GetCachedValue(key string) (any, bool)
+	SetCachedValue(key string, value any)
+	GetCachedValues(keys []string) (map[string]any, bool)
+	SetCachedValues(keyValues map[string]any)
 	HashTxBytes(txBytes []byte) []byte
+	GetCCIPPackageId(ctx context.Context, offRampPackageID string, signerAddress string) (string, error)
+	GetValuesFromPackageOwnedObjectField(ctx context.Context, packageId string, moduleId string, objectName string, fieldKeys []string) (map[string]string, error)
 }
 
 // PTBClient implements SuiClient interface using the blockvision SDK
@@ -67,8 +80,11 @@ type PTBClient struct {
 	keystoreService    loop.Keystore
 	rateLimiter        *semaphore.Weighted
 	defaultRequestType TransactionRequestType
+
 	// map of module name to normalized module definition (similar to an ABI)
 	normalizedModules map[string]map[string]models.GetNormalizedMoveModuleResponse
+
+	cache *cache.Cache // used for caching object IDs (e.g. offramp state object ID or state pointers)
 }
 
 var _ SuiPTBClient = (*PTBClient)(nil)
@@ -99,6 +115,7 @@ func NewPTBClient(
 		rateLimiter:        semaphore.NewWeighted(maxConcurrentRequests),
 		defaultRequestType: defaultRequestType,
 		normalizedModules:  make(map[string]map[string]models.GetNormalizedMoveModuleResponse),
+		cache:              cache.New(DefaultCacheExpiration, DefaultCacheCleanupInterval),
 	}, nil
 }
 
@@ -843,4 +860,85 @@ func (c *PTBClient) GetLatestPackageId(ctx context.Context, packageId string, mo
 
 func (c *PTBClient) GetClient() sui.ISuiAPI {
 	return c.client
+}
+
+func (c *PTBClient) GetCache() *cache.Cache {
+	return c.cache
+}
+
+func (c *PTBClient) GetCachedValue(key string) (any, bool) {
+	return c.cache.Get(key)
+}
+
+func (c *PTBClient) GetCachedValues(keys []string) (map[string]any, bool) {
+	result := make(map[string]any)
+	for _, key := range keys {
+		if value, found := c.cache.Get(key); found {
+			result[key] = value
+		} else {
+			return nil, false
+		}
+	}
+	return result, true
+}
+
+func (c *PTBClient) SetCachedValue(key string, value any) {
+	c.cache.Set(key, value, cache.NoExpiration)
+}
+
+func (c *PTBClient) SetCachedValues(keyValues map[string]any) {
+	for key, value := range keyValues {
+		c.cache.Set(key, value, cache.NoExpiration)
+	}
+}
+
+// GetCCIPPackageId gets the CCIP package ID from the offramp package ID.
+// IMPORTANT: This function expects to call the original (un-upgraded / first version) offramp package ID.
+func (c *PTBClient) GetCCIPPackageId(ctx context.Context, offRampPackageID string, signerAddress string) (string, error) {
+	offRamp, err := module_offramp.NewOfframp(offRampPackageID, c.GetClient())
+	if err != nil {
+		return "", err
+	}
+
+	devInspectSigner := suiSigner.NewDevInspectSigner(signerAddress)
+
+	ccipPkgId, err := offRamp.DevInspect().GetCcipPackageId(ctx, &bind.CallOpts{
+		Signer:           devInspectSigner,
+		WaitForExecution: true,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return ccipPkgId, nil
+}
+
+// GetValueFromPackageOwnedObjectField gets the value of a field from a package owned object.
+// This is used to get addresses stored within pointer objects on-chain. For example, the state object ID of a package is stored in the pointer object,
+// so we need to get the value of the pointer object's field to get the state object ID.
+func (c *PTBClient) GetValuesFromPackageOwnedObjectField(ctx context.Context, packageId string, moduleId string, objectName string, fieldKeys []string) (map[string]string, error) {
+	ownedObjects, err := c.ReadOwnedObjects(ctx, packageId, nil)
+	if err != nil {
+		c.log.Errorw("Error reading owned objects", "error", err)
+		return nil, err
+	}
+
+	foundValues := make(map[string]string)
+	for _, ownedObject := range ownedObjects {
+		qualifiedName := fmt.Sprintf("%s::%s::%s", packageId, moduleId, objectName)
+		if ownedObject.Data.Type != "" && ownedObject.Data.Type == qualifiedName {
+			// parse the object into a map
+			parsedObject := ownedObject.Data.Content.Fields
+			for _, fieldKey := range fieldKeys {
+				fieldValue, ok := parsedObject[fieldKey].(string)
+				if !ok {
+					return nil, fmt.Errorf("field %s not found in object %s", fieldKey, qualifiedName)
+				}
+
+				foundValues[fieldKey] = fieldValue
+			}
+		}
+	}
+
+	return foundValues, nil
 }
