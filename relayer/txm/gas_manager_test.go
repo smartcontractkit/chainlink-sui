e414ff72642fb96b1deaca0c9d0db9d213baec68
stackman27 <sishir.giri@smartcontract.com>
2025-09-23 20:50:04 -0700

    nit

--- relayer/txm/gas_manager_test.go
+++ relayer/txm/gas_manager_test.go
@@ -206,7 +206,7 @@ func TestSuiGasManager_CalculateOfframpExecuteGasBudget(t *testing.T) {
 				},
 			},
 			expectedGas:   nil,
-			expectedError: errors.New("gasLimit in ExtraArgsDecoded is not *big.Int or uint64, got string"),
+			expectedError: errors.New("gasLimit is not *big.Int or uint64, got string"),
 		},
 		{
 			name: "error when destGasAmount is not uint64",
