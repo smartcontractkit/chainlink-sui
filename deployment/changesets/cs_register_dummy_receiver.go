f4e783d2fba6bb742dd938b41a3fab6cc1403835
stackman27 <sishir.giri@smartcontract.com>
2025-09-29 13:10:56 -0700

    register reciever cs

---
+++ deployment/changesets/cs_register_dummy_receiver.go
@@ -0,0 +1,62 @@
+package changesets
+
+import (
+	"fmt"
+
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	"github.com/smartcontractkit/chainlink-sui/bindings/bind"
+	sui_ops "github.com/smartcontractkit/chainlink-sui/deployment/ops"
+	ccipops "github.com/smartcontractkit/chainlink-sui/deployment/ops/ccip"
+)
+
+type RegisterDummyReceiverConfig struct {
+	SuiChainSelector       uint64
+	CCIPObjectRefObjectId  string
+	DummyReceiverPackageId string
+}
+
+var _ cldf.ChangeSetV2[RegisterDummyReceiverConfig] = RegisterDummyReciever{}
+
+type RegisterDummyReciever struct{}
+
+// Apply implements deployment.ChangeSetV2.
+func (d RegisterDummyReciever) Apply(e cldf.Environment, config RegisterDummyReceiverConfig) (cldf.ChangesetOutput, error) {
+	ab := cldf.NewMemoryAddressBook()
+	seqReports := make([]operations.Report[any, any], 0)
+
+	suiChain := e.BlockChains.SuiChains()[config.SuiChainSelector]
+
+	deps := sui_ops.OpTxDeps{
+		Client: suiChain.Client,
+		Signer: suiChain.Signer,
+		GetCallOpts: func() *bind.CallOpts {
+			b := uint64(400_000_000)
+			return &bind.CallOpts{
+				WaitForExecution: true,
+				GasBudget:        &b,
+			}
+		},
+	}
+
+	// Run RegisterReciever Operation
+	RegisterRecieverOp, err := operations.ExecuteOperation(e.OperationsBundle, ccipops.RegisterDummyReceiverOp, deps, ccipops.RegisterDummyReceiverInput{
+		CCIPObjectRefObjectId:  config.CCIPObjectRefObjectId,
+		DummyReceiverPackageId: config.DummyReceiverPackageId,
+	})
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to register receiver for Sui chain %d: %w", config.SuiChainSelector, err)
+	}
+
+	seqReports = append(seqReports, []operations.Report[any, any]{RegisterRecieverOp.ToGenericReport()}...)
+
+	return cldf.ChangesetOutput{
+		AddressBook: ab,
+		Reports:     seqReports,
+	}, nil
+}
+
+// VerifyPreconditions implements deployment.ChangeSetV2.
+func (d RegisterDummyReciever) VerifyPreconditions(e cldf.Environment, config RegisterDummyReceiverConfig) error {
+	return nil
+}
