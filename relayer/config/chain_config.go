db5fa454fb5110bfabb1d00df3882a29590a4e48
stackman27 <sishir.giri@smartcontract.com>
2025-10-21 15:14:55 -0700

    bump concurrent request

--- relayer/config/chain_config.go
+++ relayer/config/chain_config.go
@@ -9,7 +9,7 @@ import (
 // Default constant values
 const (
 	DefaultBroadcastChannelSize       = uint64(4096)
-	DefaultMaxConcurrentRequests      = int64(100)
+	DefaultMaxConcurrentRequests      = int64(250)
 	DefaultRetryCount                 = int64(5)
 	DefaultMaxGasLimit                = int64(10000000)
 	DefaultTxTimeoutSeconds           = 120
