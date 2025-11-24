ec91dbc8d02e3f6f7cc3b4bf8879b17fc51fc9cd
Faisal <faisal.altameemi@smartcontract.com>
2025-11-24 23:30:25 +0400

    Update sui.nix

--- sui.nix
+++ sui.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
   src = if stdenv.hostPlatform.isDarwin then
     pkgs.fetchzip {
       url = "https://github.com/MystenLabs/sui/releases/download/mainnet-v${version}/sui-mainnet-v${version}-macos-arm64.tgz"; # Assume is a M1 Mac
-      sha256 = "sha256-0w8YYrDDbf0tWom2asp0BkzuqLmWbdcUAAkMAll7Q6Y=";  # Should be replaced when bumping versions
+      sha256 = "sha256-/rcMuLEgqV9UA0K7VaS+wIpX+jQKDQE+U1vGb5spJmU=";  # Should be replaced when bumping versions
       stripRoot = false;
     }
     else if stdenv.isLinux then
