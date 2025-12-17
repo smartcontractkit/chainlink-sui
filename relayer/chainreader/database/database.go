f5dd85f096aca227b563fc66631c2015cf975a38
Faisal <faisal.altameemi@smartcontract.com>
2025-12-17 14:16:45 +0400

    Update relayer/chainreader/database/database.go
    Co-authored-by: Sishir Giri <sishir.giri@smartcontract.com>

--- relayer/chainreader/database/database.go
+++ relayer/chainreader/database/database.go
@@ -119,7 +119,7 @@ func (store *DBStore) QueryEvents(ctx context.Context, eventAccountAddress, even
 		baseSQL += " ORDER BY (block_height, event_offset) " + direction
 	} else {
 		// default to descending order if no sort is provided
-		baseSQL += " ORDER BY (block_height, event_offset) DESC"
+		baseSQL += " ORDER BY (block_height::BIGINT, event_offset) DESC"
 	}
 
 	if limitAndSort.Limit.Count > 0 {
