28bcd4a965d62f57de11363348a2faa44612c268
faisal-link <faisal.altameemi@smartcontract.com>
2025-12-17 14:22:49 +0400

    query edit

--- relayer/chainreader/database/database.go
+++ relayer/chainreader/database/database.go
@@ -120,7 +120,7 @@ func (store *DBStore) QueryEvents(ctx context.Context, eventAccountAddress, even
 		if sortDir, ok := limitAndSort.SortBy[0].(query.SortBySequence); ok && sortDir.GetDirection() == query.Desc {
 			direction = "DESC"
 		}
-		baseSQL += " ORDER BY (block_height, event_offset) " + direction
+		baseSQL += " ORDER BY (block_height::BIGINT, event_offset) " + direction
 	} else {
 		// default to descending order if no sort is provided
 		baseSQL += " ORDER BY (block_height::BIGINT, event_offset) DESC"
