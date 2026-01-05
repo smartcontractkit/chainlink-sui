c25bd285f1395a948a45c3b44c3338674b92b488
faisal-link <faisal.altameemi@smartcontract.com>
2026-01-05 21:24:56 +0400

    remove duplicates unique index

--- relayer/chainreader/database/database_queries.go
+++ relayer/chainreader/database/database_queries.go
@@ -32,7 +32,6 @@ const (
 	CREATE INDEX IF NOT EXISTS idx_events_account_handle_timestamp ON sui.events(event_account_address, event_handle, block_timestamp DESC);
 	CREATE INDEX IF NOT EXISTS idx_events_offset ON sui.events(event_account_address, event_handle, event_offset);
 	CREATE INDEX IF NOT EXISTS idx_events_data_gin ON sui.events USING gin(data);
-	CREATE UNIQUE INDEX IF NOT EXISTS idx_events_unique ON sui.events(event_handle, tx_digest, md5(data::text));
 	CREATE INDEX IF NOT EXISTS idx_events_account_handle_id ON sui.events(event_account_address, event_handle, id DESC);
 	`
 
