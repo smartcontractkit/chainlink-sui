cd4e8542d0253e7dca0a5cfdda24a56c6deb4220
faisal-link <faisal.altameemi@smartcontract.com>
2026-01-06 17:17:50 +0400

    edits

--- relayer/chainreader/indexer/events_indexer.go
+++ relayer/chainreader/indexer/events_indexer.go
@@ -243,22 +243,26 @@ func (eIndexer *EventsIndexer) SyncEvent(ctx context.Context, selector *client.E
 		}
 
 		if dbOffsetCursor != nil {
-			txDigestBytes, err := hex.DecodeString(strings.TrimPrefix(dbOffsetCursor.TxDigest, "0x"))
-			if err != nil {
-				eIndexer.logger.Debugw("syncEvent: failed to decode tx digest, attempting base58 decoding as fallback...", "error", err, "txDigest", dbOffsetCursor.TxDigest)
-
-				// attempt to parse the tx digest from base58 as a fallback to handle older records
-				txDigestBytes, err = base58.Decode(dbOffsetCursor.TxDigest)
+			// Some DB records have hex formatted txDigest while newer entries have base58 formatted txDigest.
+			// We check if the txDigest is hex formatted and decode it if needed for backwards compatibility.
+			if strings.ToLower(dbOffsetCursor.TxDigest[:2]) == "0x" {
+				txDigestBytes, err := hex.DecodeString(dbOffsetCursor.TxDigest[2:])
 				if err != nil {
 					eIndexer.cursorMutex.RUnlock()
-					eIndexer.logger.Errorw("syncEvent: failed to decode tx digest from base58 (fallback)", "error", err, "txDigest", dbOffsetCursor.TxDigest)
+					eIndexer.logger.Errorw("syncEvent: failed to decode tx digest", "error", err, "txDigest", dbOffsetCursor.TxDigest)
 					return fmt.Errorf("syncEvent: failed to decode tx digest: %w", err)
 				}
-			}
-			// convert the db offset cursor digest from hex (the format stored in the DB) to base58 (the format expected by the client)
-			cursor = &models.EventId{
-				TxDigest: base58.Encode(txDigestBytes),
-				EventSeq: dbOffsetCursor.EventSeq,
+				// convert the db offset cursor digest from hex (the format stored in the DB) to base58 (the format expected by the client)
+				cursor = &models.EventId{
+					TxDigest: base58.Encode(txDigestBytes),
+					EventSeq: dbOffsetCursor.EventSeq,
+				}
+			} else {
+				// DB already has base58
+				cursor = &models.EventId{
+					TxDigest: dbOffsetCursor.TxDigest,
+					EventSeq: dbOffsetCursor.EventSeq,
+				}
 			}
 
 			totalCount = dbTotalCount
