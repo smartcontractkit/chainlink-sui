b57f94c4524723c38181f98a68e817859e5f4b73
faisal-link <faisal.altameemi@smartcontract.com>
2026-01-08 17:40:39 +0400

    minor refactoring

--- relayer/chainreader/indexer/events_indexer.go
+++ relayer/chainreader/indexer/events_indexer.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink-sui/relayer/chainreader/database"
 	"github.com/smartcontractkit/chainlink-sui/relayer/client"
+	"github.com/smartcontractkit/chainlink-sui/relayer/common"
 )
 
 type EventsIndexer struct {
@@ -156,55 +157,6 @@ func (eIndexer *EventsIndexer) SyncAllEvents(ctx context.Context) error {
 	eIndexer.logger.Infow("SyncAllEvents: successfully synced all events", "count", successCount)
 
 	return nil
-}
-
-// Converts snake_case to camelCase
-func snakeToCamel(s string) string {
-	parts := strings.Split(s, "_")
-	for i := range parts {
-		if i > 0 && len(parts[i]) > 0 {
-			parts[i] = strings.ToUpper(string(parts[i][0])) + parts[i][1:]
-		}
-	}
-
-	return strings.Join(parts, "")
-}
-
-// Recursively convert all keys in the map to camelCase,
-// with a special case for message.header.sequence_number → seqNum
-func convertMapKeysToCamelCase(input any) any {
-	return convertMapKeysToCamelCaseWithPath(input, "")
-}
-
-func convertMapKeysToCamelCaseWithPath(input any, path string) any {
-	switch typed := input.(type) {
-	case map[string]any:
-		result := make(map[string]any)
-		for k, v := range typed {
-			camelKey := snakeToCamel(k)
-			fullPath := path
-			if fullPath != "" {
-				fullPath += "." + camelKey
-			} else {
-				fullPath = camelKey
-			}
-
-			if fullPath == "message.header.sequenceNumber" {
-				camelKey = "seqNum"
-			}
-
-			result[camelKey] = convertMapKeysToCamelCaseWithPath(v, fullPath)
-		}
-
-		return result
-
-	case []any:
-		for i, v := range typed {
-			typed[i] = convertMapKeysToCamelCaseWithPath(v, path)
-		}
-	}
-
-	return input
 }
 
 func (eIndexer *EventsIndexer) SyncEvent(ctx context.Context, selector *client.EventSelector) error {
@@ -347,7 +299,7 @@ eventLoop:
 				offset += uint64(i) + totalCount
 
 				// normalize the data, convert snake case to camel case
-				normalizedData := convertMapKeysToCamelCase(event.ParsedJson)
+				normalizedData := common.ConvertMapKeysToCamelCase(event.ParsedJson)
 
 				// Convert the txDigest to hex
 				txDigestHex := event.Id.TxDigest
