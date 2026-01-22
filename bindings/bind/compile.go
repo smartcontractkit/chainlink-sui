fc85198e9de8226bb627c3b1043d375c1d670a49
FelixFan1992 <fankejin@gmail.com>
2026-01-22 12:39:44 -0500

    update

--- bindings/bind/compile.go
+++ bindings/bind/compile.go
@@ -783,7 +783,7 @@ func compilePackageInternal(packageName contracts.Package, namedAddresses map[st
 
 	} else {
 		log.Printf("testing publish for package: %s\n", packageRoot)
-		cmd = exec.Command("sui", "client", "test-publish", "--environment", env, "--serialize-unsigned-transaction", "--sender", namedAddresses["signer"], "--json")
+		cmd = exec.Command("sui", "client", "test-publish", "--build-env", "testnet", "--environment", env, "--serialize-unsigned-transaction", "--sender", namedAddresses["signer"], "--json")
 		cmd.Dir = packageRoot
 		output, err := cmd.Output()
 		if err != nil {
