71438127009334e3b54307732dedc8fb41c05827
FelixFan1992 <fankejin@gmail.com>
2026-02-18 12:08:10 -0500

    update

--- sui.nix
+++ sui.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     else if stdenv.isLinux then
       pkgs.fetchzip {
         url = "https://github.com/MystenLabs/sui/releases/download/mainnet-v${version}/sui-mainnet-v${version}-ubuntu-x86_64.tgz";
-        sha256 = "sha256-iZYCVJCGrweaKoznRicrdzotaQRj94oJi83DAfzdp6U=";  # Should be replaced when bumping versions
+        sha256 = "sha256-eK8nvQfV2W8oQzOr1kNVc/3TABwvDG+6SCm/MIQpz5I=";  # Should be replaced when bumping versions
         stripRoot = false;
       }
     else
