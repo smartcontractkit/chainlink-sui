0a84b29d4d3eec9da25bfc46f3560dd9a7d5e431
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2026-03-31 18:16:33 +0200

    re-enable mcms int tests

--- integration-tests/mcms/runner_test.go
+++ integration-tests/mcms/runner_test.go
@@ -9,17 +9,17 @@ import (
 )
 
 func TestMCMSandCCIPSuite(t *testing.T) {
-	// t.Run("CCIPMCMSTestSuite", func(t *testing.T) {
-	// 	suite.Run(t, new(CCIPMCMSTestSuite))
-	// })
+	t.Run("CCIPMCMSTestSuite", func(t *testing.T) {
+		suite.Run(t, new(CCIPMCMSTestSuite))
+	})
 
-	// t.Run("TokenPoolTestSuite", func(t *testing.T) {
-	// 	suite.Run(t, new(TokenPoolTestSuite))
-	// })
+	t.Run("TokenPoolTestSuite", func(t *testing.T) {
+		suite.Run(t, new(TokenPoolTestSuite))
+	})
 
-	// t.Run("MCMSUserTestSuite", func(t *testing.T) {
-	// 	suite.Run(t, new(UpgradeTestSuite))
-	// })
+	t.Run("MCMSUserTestSuite", func(t *testing.T) {
+		suite.Run(t, new(UpgradeTestSuite))
+	})
 
 	t.Run("CCIPCurseMCMSSuite", func(t *testing.T) {
 		suite.Run(t, new(CCIPCurseMCMSTestSuite))
