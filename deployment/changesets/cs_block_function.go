965a09ab8ec278ed6efe1654224d8530111a2b66
FelixFan1992 <fankejin@gmail.com>
2026-04-03 13:53:47 -0400

    fix ops for add package id and block funcs (#364)

--- deployment/changesets/cs_block_function.go
+++ deployment/changesets/cs_block_function.go
@@ -5,19 +5,22 @@ import (
 
 	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	"github.com/smartcontractkit/mcms"
+
 	"github.com/smartcontractkit/chainlink-sui/bindings/bind"
+	"github.com/smartcontractkit/chainlink-sui/deployment"
 	sui_ops "github.com/smartcontractkit/chainlink-sui/deployment/ops"
 	ccipops "github.com/smartcontractkit/chainlink-sui/deployment/ops/ccip"
+	mcmsops "github.com/smartcontractkit/chainlink-sui/deployment/ops/mcms"
+	"github.com/smartcontractkit/chainlink-sui/deployment/utils"
 )
 
 type BlockFunctionConfig struct {
-	SuiChainSelector      uint64 `yaml:"suiChainSelector"`
-	CCIPPackageId         string `yaml:"ccipPackageId"`
-	CCIPObjectRefObjectId string `yaml:"ccipObjectRefObjectId"`
-	OwnerCapObjectId      string `yaml:"ownerCapObjectId"`
-	ModuleName            string `yaml:"moduleName"`
-	FunctionName          string `yaml:"functionName"`
-	Version               uint8  `yaml:"version"`
+	SuiChainSelector uint64                `yaml:"suiChainSelector"`
+	ModuleName       string                `yaml:"moduleName"`
+	FunctionName     string                `yaml:"functionName"`
+	Version          uint8                 `yaml:"version"`
+	TimelockConfig   *utils.TimelockConfig `yaml:"timelockConfig,omitempty"`
 }
 
 var _ cldf.ChangeSetV2[BlockFunctionConfig] = BlockFunction{}
@@ -25,8 +28,12 @@ var _ cldf.ChangeSetV2[BlockFunctionConfig] = BlockFunction{}
 type BlockFunction struct{}
 
 func (d BlockFunction) Apply(e cldf.Environment, config BlockFunctionConfig) (cldf.ChangesetOutput, error) {
-	ab := cldf.NewMemoryAddressBook()
+	state, err := deployment.LoadOnchainStatesui(e)
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to load onchain state: %w", err)
+	}
 
+	chainState := state[config.SuiChainSelector]
 	suiChain := e.BlockChains.SuiChains()[config.SuiChainSelector]
 
 	deps := sui_ops.OpTxDeps{
@@ -42,10 +49,14 @@ func (d BlockFunction) Apply(e cldf.Environment, config BlockFunctionConfig) (cl
 		SuiRPC: suiChain.URL,
 	}
 
+	if config.TimelockConfig != nil {
+		deps.Signer = nil
+	}
+
 	report, err := operations.ExecuteOperation(e.OperationsBundle, ccipops.BlockFunctionOp, deps, ccipops.BlockFunctionInput{
-		CCIPPackageId:    config.CCIPPackageId,
-		StateObjectId:    config.CCIPObjectRefObjectId,
-		OwnerCapObjectId: config.OwnerCapObjectId,
+		CCIPPackageId:    chainState.CCIPAddress,
+		StateObjectId:    chainState.CCIPObjectRef,
+		OwnerCapObjectId: chainState.CCIPOwnerCapObjectId,
 		ModuleName:       config.ModuleName,
 		FunctionName:     config.FunctionName,
 		Version:          config.Version,
@@ -54,9 +65,30 @@ func (d BlockFunction) Apply(e cldf.Environment, config BlockFunctionConfig) (cl
 		return cldf.ChangesetOutput{}, fmt.Errorf("failed to block function for Sui chain %d: %w", config.SuiChainSelector, err)
 	}
 
+	if config.TimelockConfig != nil {
+		mcmsConfig := mcmsops.ProposalGenerateInput{
+			ChainSelector:      config.SuiChainSelector,
+			Defs:               []operations.Definition{report.Def},
+			Inputs:             []any{report.Input},
+			MmcsPackageID:      chainState.MCMSPackageID,
+			McmsStateObjID:     chainState.MCMSStateObjectID,
+			TimelockObjID:      chainState.MCMSTimelockObjectID,
+			AccountObjID:       chainState.MCMSAccountStateObjectID,
+			RegistryObjID:      chainState.MCMSRegistryObjectID,
+			DeployerStateObjID: chainState.MCMSDeployerStateObjectID,
+			TimelockConfig:     *config.TimelockConfig,
+		}
+		result, err := operations.ExecuteSequence(e.OperationsBundle, mcmsops.MCMSDynamicProposalGenerateSeq, deps, mcmsConfig)
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to generate MCMS proposal: %w", err)
+		}
+		return cldf.ChangesetOutput{
+			MCMSTimelockProposals: []mcms.TimelockProposal{result.Output},
+		}, nil
+	}
+
 	return cldf.ChangesetOutput{
-		AddressBook: ab,
-		Reports:     []operations.Report[any, any]{report.ToGenericReport()},
+		Reports: []operations.Report[any, any]{report.ToGenericReport()},
 	}, nil
 }
 
