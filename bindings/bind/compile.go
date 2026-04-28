ae8a03732b28da52757f72a6463cf233b2733975
FelixFan1992 <fankejin@gmail.com>
2026-04-28 17:35:57 -0400

    update sui version

--- bindings/bind/compile.go
+++ bindings/bind/compile.go
@@ -942,7 +942,7 @@ func getDynamicSuiRPC() (string, error) {
 		return envRPC, nil
 	}
 
-	cmd := exec.Command("docker", "ps", "--filter", "ancestor=mysten/sui-tools:mainnet-v1.67.3", "--format", "{{.Ports}}")
+	cmd := exec.Command("docker", "ps", "--filter", "ancestor=mysten/sui-tools:mainnet-v1.70.2", "--format", "{{.Ports}}")
 	out, err := cmd.Output()
 	if err != nil {
 		return "", fmt.Errorf("docker ps failed: %w", err)
