1e85144a6d9a77bad13086c5c8581473bf925635
FelixFan1992 <fankejin@gmail.com>
2026-05-29 09:51:52 -0400

    update

---
+++ deployment/changesets/cs_deploy_broken_receiver.go
@@ -0,0 +1,70 @@
+package changesets
+
+import (
+	"fmt"
+
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	"github.com/smartcontractkit/chainlink-sui/bindings/bind"
+	"github.com/smartcontractkit/chainlink-sui/deployment"
+	sui_ops "github.com/smartcontractkit/chainlink-sui/deployment/ops"
+	ccipops "github.com/smartcontractkit/chainlink-sui/deployment/ops/ccip"
+)
+
+type DeployBrokenReceiverConfig struct {
+	SuiChainSelector uint64
+	McmsOwner        string
+}
+
+var _ cldf.ChangeSetV2[DeployBrokenReceiverConfig] = DeployBrokenReceiver{}
+
+// DeployBrokenReceiver deploys the test-only CCIP broken receiver package, whose
+// generic ccip_receive<T> produces the poison normalized ABI from report 71024.
+type DeployBrokenReceiver struct{}
+
+// Apply implements deployment.ChangeSetV2.
+func (d DeployBrokenReceiver) Apply(e cldf.Environment, config DeployBrokenReceiverConfig) (cldf.ChangesetOutput, error) {
+	state, err := deployment.LoadOnchainStatesui(e)
+	if err != nil {
+		return cldf.ChangesetOutput{}, err
+	}
+
+	ab := cldf.NewMemoryAddressBook()
+	seqReports := make([]operations.Report[any, any], 0)
+
+	suiChain := e.BlockChains.SuiChains()[config.SuiChainSelector]
+
+	deps := sui_ops.OpTxDeps{
+		Client: suiChain.Client,
+		Signer: suiChain.Signer,
+		GetCallOpts: func() *bind.CallOpts {
+			b := uint64(400_000_000)
+			return &bind.CallOpts{
+				WaitForExecution: true,
+				GasBudget:        &b,
+			}
+		},
+		SuiRPC: suiChain.URL,
+	}
+
+	DeployBrokenReceiverOp, err := operations.ExecuteOperation(e.OperationsBundle, ccipops.DeployCCIPBrokenReceiverOp, deps, ccipops.DeployBrokenReceiverInput{
+		CCIPPackageId: state[config.SuiChainSelector].CCIPAddress,
+		McmsPackageId: state[config.SuiChainSelector].MCMSPackageID,
+		McmsOwner:     config.McmsOwner,
+	})
+	if err != nil {
+		return cldf.ChangesetOutput{}, fmt.Errorf("failed to deploy broken receiver for Sui chain %d: %w", config.SuiChainSelector, err)
+	}
+
+	seqReports = append(seqReports, []operations.Report[any, any]{DeployBrokenReceiverOp.ToGenericReport()}...)
+
+	return cldf.ChangesetOutput{
+		AddressBook: ab,
+		Reports:     seqReports,
+	}, nil
+}
+
+// VerifyPreconditions implements deployment.ChangeSetV2.
+func (d DeployBrokenReceiver) VerifyPreconditions(e cldf.Environment, config DeployBrokenReceiverConfig) error {
+	return nil
+}
