91f78697fe8ba83b9b3628b0594e9be87e4bd22a
FelixFan1992 <fankejin@gmail.com>
2026-05-30 13:06:57 -0400

    fix sha

--- sui.nix
+++ sui.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     else if stdenv.isLinux then
       pkgs.fetchzip {
         url = "https://github.com/MystenLabs/sui/releases/download/mainnet-v${version}/sui-mainnet-v${version}-ubuntu-x86_64.tgz";
-        sha256 = "sha256-iOhVRR4tZJiOmQJmvcxQxEgq0cfdqtI/NYObZ+NU4GY=";  # Should be replaced when bumping versions
+        sha256 = "sha256-zsedBVNaQSISvY5WdDhh/m61BuzuUPpZ1nnbWKtHRu0=";  # Should be replaced when bumping versions
         stripRoot = false;
       }
     else
