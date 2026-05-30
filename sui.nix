5ce119a46373a9580e52a305e08c31419054e203
FelixFan1992 <fankejin@gmail.com>
2026-05-30 12:38:25 -0400

    update sui version to 1.72.5

--- sui.nix
+++ sui.nix
@@ -4,7 +4,7 @@
 }:
 stdenv.mkDerivation rec {
   name = "sui-cli-${version}";
-  version = "1.72.2"; # Update as needed. Should be a mainnet release version from https://github.com/MystenLabs/sui/releases
+    version = "1.72.5"; # Update as needed. Should be a mainnet release version from https://github.com/MystenLabs/sui/releases
 
   src = if stdenv.hostPlatform.isDarwin then
     pkgs.fetchzip {
