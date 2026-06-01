26c1367df935de37dd96f5fd33f161fc45ffb741
FelixFan1992 <fankejin@gmail.com>
2026-06-01 10:47:51 -0400

    update sui version to 1.72.5 (#399)
    * update sui version to 1.72.5
    
    * fix sha
    
    * one more
    
    * fix

--- sui.nix
+++ sui.nix
@@ -4,7 +4,7 @@
 }:
 stdenv.mkDerivation rec {
   name = "sui-cli-${version}";
-  version = "1.72.2"; # Update as needed. Should be a mainnet release version from https://github.com/MystenLabs/sui/releases
+  version = "1.72.5"; # Update as needed. Should be a mainnet release version from https://github.com/MystenLabs/sui/releases
 
   src = if stdenv.hostPlatform.isDarwin then
     pkgs.fetchzip {
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     else if stdenv.isLinux then
       pkgs.fetchzip {
         url = "https://github.com/MystenLabs/sui/releases/download/mainnet-v${version}/sui-mainnet-v${version}-ubuntu-x86_64.tgz";
-        sha256 = "sha256-iOhVRR4tZJiOmQJmvcxQxEgq0cfdqtI/NYObZ+NU4GY=";  # Should be replaced when bumping versions
+        sha256 = "sha256-zsedBVNaQSISvY5WdDhh/m61BuzuUPpZ1nnbWKtHRu0=";  # Should be replaced when bumping versions
         stripRoot = false;
       }
     else
