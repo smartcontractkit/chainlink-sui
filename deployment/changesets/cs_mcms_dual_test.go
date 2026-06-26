f589ad80e2538dbeaa731da88d4b4976b837adb8
FelixFan1992 <fankejin@gmail.com>
2026-06-26 19:12:53 -0400

    test

--- deployment/changesets/cs_mcms_dual_test.go
+++ deployment/changesets/cs_mcms_dual_test.go
@@ -51,6 +51,57 @@ func TestDeployMCMS_VerifyPreconditions_RejectsDuplicateInstance(t *testing.T) {
 	require.Contains(t, err.Error(), "slow MCMS is already recorded")
 }
 
+func TestDeployMCMS_VerifyPreconditions_RejectsDuplicateFastInstance(t *testing.T) {
+	t.Parallel()
+
+	selector := cselectors.SUI_TESTNET.Selector
+	ab := cldf.NewMemoryAddressBook()
+	require.NoError(t, deployment.StoreMCMSInAddressBook(ab, selector, mcmsops.DeployMCMSSeqOutput{
+		PackageId: "0xfast_pkg",
+		Objects: mcmsops.DeployMCMSObjects{
+			McmsMultisigStateObjectId:   "0xfast_state",
+			McmsRegistryObjectId:        "0xfast_registry",
+			McmsAccountStateObjectId:    "0xfast_account",
+			McmsAccountOwnerCapObjectId: "0xfast_owner_cap",
+			TimelockObjectId:            "0xfast_timelock",
+			McmsDeployerStateObjectId:   "0xfast_deployer",
+		},
+	}, deployment.MCMSInstanceFastCurse))
+
+	cs := DeployMCMS{}
+	err := cs.VerifyPreconditions(dualMCMSEnv(t, ab, selector), DeployMCMSConfig{
+		DeployMCMSSeqInput: mcmsops.DeployMCMSSeqInput{ChainSelector: selector},
+		IsFastCurse:        true,
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "fastcurse MCMS is already recorded")
+}
+
+func TestDeployMCMS_VerifyPreconditions_AllowsFastWhenOnlySlowExists(t *testing.T) {
+	t.Parallel()
+
+	selector := cselectors.SUI_TESTNET.Selector
+	ab := cldf.NewMemoryAddressBook()
+	require.NoError(t, deployment.StoreMCMSInAddressBook(ab, selector, mcmsops.DeployMCMSSeqOutput{
+		PackageId: "0xslow_pkg",
+		Objects: mcmsops.DeployMCMSObjects{
+			McmsMultisigStateObjectId:   "0xslow_state",
+			McmsRegistryObjectId:        "0xslow_registry",
+			McmsAccountStateObjectId:    "0xslow_account",
+			McmsAccountOwnerCapObjectId: "0xslow_owner_cap",
+			TimelockObjectId:            "0xslow_timelock",
+			McmsDeployerStateObjectId:   "0xslow_deployer",
+		},
+	}, deployment.MCMSInstanceSlow))
+
+	cs := DeployMCMS{}
+	err := cs.VerifyPreconditions(dualMCMSEnv(t, ab, selector), DeployMCMSConfig{
+		DeployMCMSSeqInput: mcmsops.DeployMCMSSeqInput{ChainSelector: selector},
+		IsFastCurse:        true,
+	})
+	require.NoError(t, err)
+}
+
 func TestRegisterCurserCap_VerifyPreconditions_RequiresBothMCMSInstances(t *testing.T) {
 	t.Parallel()
 
