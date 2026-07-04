1f54007b6561ab23c1d8f15d5f08770199399c08
FelixFan1992 <fankejin@gmail.com>
2026-07-04 15:12:30 -0400

    more fast curse tests (#437)
    * more fast curse tests
    
    * ownership transfer

--- deployment/adapters/fastcurse_test.go
+++ deployment/adapters/fastcurse_test.go
@@ -38,6 +38,18 @@ func fastCurseTestBundle(t *testing.T) cld_ops.Bundle {
 	)
 }
 
+func uncurseTestBundle(t *testing.T) cld_ops.Bundle {
+	t.Helper()
+
+	registry := cld_ops.NewOperationRegistry(rmnops.UncurseChainOp.AsUntyped())
+	return cld_ops.NewBundle(
+		t.Context,
+		logger.Test(t),
+		cld_ops.NewMemoryReporter(),
+		cld_ops.WithOperationRegistry(registry),
+	)
+}
+
 func txAdditionalFields(t *testing.T, tx mcmstypes.Transaction) suisdk.AdditionalFields {
 	t.Helper()
 
@@ -138,4 +150,106 @@ func TestCurseAdapter_Curse_WithUpgradedPackage_PropagatesLatestPackageID(t *tes
 	latestPackageID, err := suideployutils.TransactionLatestPackageID(tx)
 	require.NoError(t, err)
 	require.Equal(t, testLatestCCIPPackageID, latestPackageID)
+}
+
+func TestCurseAdapter_Uncurse_UsesOwnerCapSequence(t *testing.T) {
+	t.Parallel()
+
+	selector := cselectors.SUI_TESTNET.Selector
+	adapter := adapters.NewCurseAdapter()
+	adapter.CCIPAddress = testCCIPPackageID
+	adapter.CCIPObjectRef = testCCIPObjectRef
+	adapter.CCIPOwnerCapObjectID = "0x3333333333333333333333333333333333333333333333333333333333333333"
+
+	chains := cldf_chain.NewBlockChains(map[uint64]cldf_chain.BlockChain{
+		selector: cldfsui.Chain{ChainMetadata: cldfsui.ChainMetadata{Selector: selector}},
+	})
+
+	report, err := cld_ops.ExecuteSequence(
+		uncurseTestBundle(t),
+		adapter.Uncurse(),
+		chains,
+		fastcurse.CurseInput{
+			ChainSelector: selector,
+			Subjects:      []fastcurse.Subject{fastcurse.GlobalCurseSubject()},
+		},
+	)
+	require.NoError(t, err)
+	require.Len(t, report.Output.BatchOps, 1)
+	require.Len(t, report.Output.BatchOps[0].Transactions, 1)
+
+	tx := report.Output.BatchOps[0].Transactions[0]
+	require.Equal(t, testCCIPPackageID, tx.To)
+
+	fields := txAdditionalFields(t, tx)
+	require.Equal(t, "rmn_remote", fields.ModuleName)
+	require.Equal(t, "uncurse_multiple", fields.Function)
+}
+
+func TestCurseAdapter_Uncurse_WithUpgradedPackage_PropagatesLatestPackageID(t *testing.T) {
+	t.Parallel()
+
+	selector := cselectors.SUI_TESTNET.Selector
+	adapter := adapters.NewCurseAdapter()
+	adapter.CCIPAddress = testCCIPPackageID
+	adapter.LatestCCIPPackageID = testLatestCCIPPackageID
+	adapter.CCIPObjectRef = testCCIPObjectRef
+	adapter.CCIPOwnerCapObjectID = "0x3333333333333333333333333333333333333333333333333333333333333333"
+
+	chains := cldf_chain.NewBlockChains(map[uint64]cldf_chain.BlockChain{
+		selector: cldfsui.Chain{ChainMetadata: cldfsui.ChainMetadata{Selector: selector}},
+	})
+
+	report, err := cld_ops.ExecuteSequence(
+		uncurseTestBundle(t),
+		adapter.Uncurse(),
+		chains,
+		fastcurse.CurseInput{
+			ChainSelector: selector,
+			Subjects:      []fastcurse.Subject{fastcurse.GenericSelectorToSubject(cselectors.ETHEREUM_MAINNET.Selector)},
+		},
+	)
+	require.NoError(t, err)
+	require.Len(t, report.Output.BatchOps, 1)
+	require.Len(t, report.Output.BatchOps[0].Transactions, 1)
+
+	tx := report.Output.BatchOps[0].Transactions[0]
+	require.Equal(t, testCCIPPackageID, tx.To)
+
+	latestPackageID, err := suideployutils.TransactionLatestPackageID(tx)
+	require.NoError(t, err)
+	require.Equal(t, testLatestCCIPPackageID, latestPackageID)
+}
+
+func TestCurseAdapter_Uncurse_MultipleSubjects(t *testing.T) {
+	t.Parallel()
+
+	selector := cselectors.SUI_TESTNET.Selector
+	adapter := adapters.NewCurseAdapter()
+	adapter.CCIPAddress = testCCIPPackageID
+	adapter.CCIPObjectRef = testCCIPObjectRef
+	adapter.CCIPOwnerCapObjectID = "0x3333333333333333333333333333333333333333333333333333333333333333"
+
+	chains := cldf_chain.NewBlockChains(map[uint64]cldf_chain.BlockChain{
+		selector: cldfsui.Chain{ChainMetadata: cldfsui.ChainMetadata{Selector: selector}},
+	})
+
+	subjects := []fastcurse.Subject{
+		fastcurse.GenericSelectorToSubject(cselectors.ETHEREUM_MAINNET.Selector),
+		fastcurse.GenericSelectorToSubject(cselectors.POLYGON_MAINNET.Selector),
+	}
+
+	report, err := cld_ops.ExecuteSequence(
+		uncurseTestBundle(t),
+		adapter.Uncurse(),
+		chains,
+		fastcurse.CurseInput{
+			ChainSelector: selector,
+			Subjects:      subjects,
+		},
+	)
+	require.NoError(t, err)
+	require.Len(t, report.Output.BatchOps, 1)
+	require.Equal(t, mcmstypes.ChainSelector(selector), report.Output.BatchOps[0].ChainSelector)
+	require.Len(t, report.Output.BatchOps[0].Transactions, 1)
 }
