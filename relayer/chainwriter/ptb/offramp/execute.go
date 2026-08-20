860b43de8de694bd7151bef42c1c545b9970111d
FelixFan1992 <fankejin@gmail.com>
2026-08-20 17:26:35 -0400

    no continue

--- relayer/chainwriter/ptb/offramp/execute.go
+++ relayer/chainwriter/ptb/offramp/execute.go
@@ -278,7 +278,6 @@ func ProcessTokenPools(
 					"tokenPool", tokenConfig.TokenPoolPackageId,
 					"coinMetadataAddress", coinMetadataAddress,
 					"error", err)
-				continue
 			}
 			return nil, fmt.Errorf("failed to append token pool command to PTB: %w", err)
 		}
